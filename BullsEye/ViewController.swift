06422c4a1004e8da56375f26d7ee27e9dfde1953
Maartz <maartz@protonmail.com>
2018-09-14 08:51:00 +0200

    feat: create bullseye layout, add slider rounded value function

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue: Int = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func showAlert(){
+        
+        let message = "The value of the slider is now \(currentValue)"
+        
+        // Create an alert pop up window
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        // Create an alert button action
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        // Add the action button to the alert window
+        alert.addAction(action)
+        // present the alert to the user
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
 
 }
 
