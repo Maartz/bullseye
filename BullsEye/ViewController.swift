cc41807cbb71fc065413c526245daa6b5d4d7b35
Maartz <maartz@protonmail.com>
2018-09-18 08:59:02 +0200

    feat: create startnewgame func, add bonus points, change popup title by context

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
 	var currentValue: Int = 0
 	var targetValue: Int = 0
 	var score: Int = 0
+	var round: Int = 0
 	
 	@IBOutlet weak var slider: UISlider!
 	@IBOutlet weak var targetLabel: UILabel!
+	@IBOutlet weak var userScore: UILabel!
+	@IBOutlet weak var userRound: UILabel!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -27,23 +30,41 @@ class ViewController: UIViewController {
 	
 	@IBAction func showAlert(){
 		
+		
 		let difference = abs(targetValue - currentValue)
-		let points = 100 - difference
+		var points = 100 - difference
 		
 		score += points
 		
-		let message = "You scored \(points) points"
+		let title: String
 		
+		if difference == 0 {
+			title = "Perfect!"
+			points += 100
+		} else if difference < 5 {
+			title = "You almost had it!"
+			if difference == 1 {
+				points += 50
+			}
+		} else if difference < 10 {
+			title = "Pretty good!"
+		} else {
+			title = "Not even close..."
+		}
+		
+		let message = "You scored \(points) points"
 		// Create an alert pop up window
-		let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
 		// Create an alert button action
-		let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+		let action = UIAlertAction(title: "Awesome", style: .default, handler: {
+			action in
+			self.startNewRound()
+		})
 		// Add the action button to the alert window
 		alert.addAction(action)
 		// present the alert to the user
 		present(alert, animated: true, completion: nil)
 		
-		startNewRound()
 	}
 	
 	@IBAction func sliderMoved(_ slider: UISlider){
@@ -55,11 +76,20 @@ class ViewController: UIViewController {
 		targetValue = Int.random(in: 1...100)
 		currentValue = 50
 		slider.value = Float(currentValue)
+		round += 1
 		updateLabels()
 	}
 	
 	func updateLabels(){
 		targetLabel.text = String(targetValue)
+		userScore.text = String(score)
+		userRound.text = String(round)
+	}
+	
+	@IBAction func startNewGame(){
+		score = 0
+		round = 0
+		startNewRound()
 	}
 	
 }
